a02db9a81461f4ac020c191f455f68f875044c3b
SONU <srishyama01@gmail.com>
2022-11-27 13:55:48 +0530

    "blank app templete for firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -38,70 +38,7 @@ class _FirebaseTutorialState extends State<FirebaseTutorial> {
       appBar: AppBar(
         title: const Text("Firebase Tutorials"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //lest place it at the top
-            Text(
-              "My name is : $name",
-              style: TextStyle(fontSize: 28),
-            ),
-            //crude operation
-            //create button
-            ElevatedButton(
-                onPressed: () {
-                  // lest creat database in firestore
-                  // name that data base users
-                  var firestore =
-                      FirebaseFirestore.instance.collection("users");
-                  // it will create a collection is firestore named users
-                  // ;ets add a document in firestore named "my doc"
-                  var store = firestore.doc('my_doc');
-                  // it will add a document in firestore's users collection
-                  // lets add users detail in this doc.
-                  store.set({
-                    //  add map values in this
-                    'name': "shyama Dev",
-                    'channel': "Youtube",
-                    'contact': "1234567"
-                  });
-                  //  it will add rcored of user
-                  // Lets save it
-                  // record save successfully
-                },
-                child: const Text("Create")),
-            //Read button
-            ElevatedButton(
-                onPressed: () {
-                  // To read data of name from user collection's my_doc documants
-                  // but first create a variable to store the value
-                  //Lets read the name Value from firestore and store it
-                  var firestore = FirebaseFirestore.instance.collection("users").doc('my_doc');
-                  firestore.get().then((value) {
-                    // after getting the doc set name = vlue['name]
-                    name = value.data()!['name'];
-                    setState(() {});
-                    // lets fetch data
-                  });
-                },
-                child: const Text("Read")),
-            //Update button
-            ElevatedButton(onPressed: () {
-              var firestore = FirebaseFirestore.instance.collection("users").doc('my_doc');
-              firestore.update({
-                 'name': "shyam prasad",
-              });
-            }, child: const Text("Update")),
-            //delete button
-            ElevatedButton(onPressed: () {
-              var firestore = FirebaseFirestore.instance.collection("users").doc('my_doc');
-              firestore.delete();
-            }, child: const Text("Delete")),
-          ],
-        ),
-      ),
-      drawer: Drawer(),
+      body: Container(),
     );
   }
 }
